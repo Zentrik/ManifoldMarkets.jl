2b0132a6472fbb9fc58d8ed90d4c3c66d705edee
Zentrik <Zentrik@users.noreply.github.com>
2023-01-31 17:39:02 +0000

    Should retry requests now

--- src/api.jl
+++ src/api.jl
@@ -13,7 +13,7 @@ function getHTTP(url; query=nothing)
         query = filter(pair -> pair.second !== nothing, query)
     end
 
-    response = HTTP.get(url, query=query, connect_timeout=15, readtimeout=15)
+    response = HTTP.get(url, query=query, connect_timeout=15, readtimeout=15, retry=true, retries=4)
     return JSON.parse(String(response.body))
 
     # slow? should be faster i think if we do it right
